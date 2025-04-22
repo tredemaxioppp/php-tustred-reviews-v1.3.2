118cd8241ff4781adf98b392dd59098b937b2a09
tredemaxioppp <44163142+tredemaxioppp@users.noreply.github.com>
2025-04-22 17:49:32 +0200

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,130 @@
+<?php return array (
+  'beyondcode/laravel-dump-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BeyondCode\\DumpServer\\DumpServerServiceProvider',
+    ),
+  ),
+  'crivion/options' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Crivion\\Options\\OptionsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Options' => 'Crivion\\Options\\Facade',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'felixkiss/uniquewith-validator' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Felixkiss\\UniqueWithValidator\\ServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'kalnoy/nestedset' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Kalnoy\\Nestedset\\NestedSetServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'rinvex/laravel-categories' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Rinvex\\Categories\\Providers\\CategoriesServiceProvider',
+    ),
+  ),
+  'rinvex/laravel-support' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Rinvex\\Support\\Providers\\SupportServiceProvider',
+    ),
+  ),
+  'spatie/laravel-schemaless-attributes' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\SchemalessAttributes\\SchemalessAttributesServiceProvider',
+    ),
+  ),
+  'spatie/laravel-translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+  'uxweb/sweet-alert' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'UxWeb\\SweetAlert\\SweetAlertServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Alert' => 'UxWeb\\SweetAlert\\SweetAlert',
+    ),
+  ),
+);
